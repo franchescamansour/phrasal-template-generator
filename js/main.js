7f753c50ac4c0d4b8f4bbeffef95cc7b131f4360
franchescamansour <cocomansour@Franchesca.local>
2016-03-01 14:17:15 -0500

    Java code

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var colour;
+var creature;
+var adjective;
+var verd;
+var story;
+
+alert('Let’s make a story together!');
+
+colour = prompt('A colour (orange, indigo, etc.)');
+creature = prompt('A creature (dragon, moth, etc.)');
+adjective = prompt('An adjective (beautiful, super, etc.)');
+verd = prompt('A past tense verb (ran, burped, etc.)');
+
+story = prompt('Which story would you like? \nEnter the letter below: \na) Snacking \nb) Escaping \nc) Winning');
+
+if (story == 'a') {
+  document.write('After snacking on ' + (adjective) + ' treats, the ' + (colour) + ' bellied ' + (creature) + ' ' + (verb) + ' for hours.');
+}
+
+if (story == 'b') {
+  document.write('Ameilia ' + (verb) + ' through the ' + (adjective) + (colour) + ' nebula escaping the space ' + (creature) + '.');
+}
+
+if (story == 'c') {
+  document.write('Jackson chose his ' + (adjective) + ', ' + (colour) + ' ' + (creature) + ' card and ' + (verb) + ' it to the table knowing he won.');
+}
